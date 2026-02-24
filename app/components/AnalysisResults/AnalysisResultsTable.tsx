36ef16310bc6c2cb76de2cd9ed5b6df99948d798
hseon2 <hseon326@gmail.com>
2026-02-24 11:19:16 +0900

    HS Increase font size and weight of date range text

--- app/components/AnalysisResults/AnalysisResultsTable.tsx
+++ app/components/AnalysisResults/AnalysisResultsTable.tsx
@@ -111,9 +111,9 @@ export function AnalysisResultsTable({
                     {country}
                     {dateInfo && (
                       <span style={{ 
-                        fontSize: '13px', 
-                        fontWeight: '400', 
-                        color: '#7f8c8d',
+                        fontSize: '15px', 
+                        fontWeight: '500', 
+                        color: '#5a6c7d',
                         marginLeft: '8px'
                       }}>
                         ({dateInfo.startDate}~{dateInfo.endDate}, {dateInfo.days} days)
